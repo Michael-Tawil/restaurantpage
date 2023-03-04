0417497660661657fa86bb4795035ecf74d51f04
michael-tawil <michaeltawil3@gmail.com>
2023-03-05 00:45:09 +1100

    added event listner to change pages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-console.log('THIS IS WORKING');
 import './style.css';
 import pageload from './pageload';
 pageload();
+const navlink = document.querySelectorAll('a');
+
+const handleinput = (e) => {
+    switch(e){
+        case e.target.href === "#Home":
+            pageload();
+            break;
+        case e.target.href === "#Menu":
+            Menuload();
+            break;
+        case e.target.href === "#Contact-Us":
+                contactusload();
+                break;            
+    }
+}
+
+navlink.forEach(link => link.addEventListener('click',handleinput));
